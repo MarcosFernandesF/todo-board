b1faffb62318ca8e9a2832be5eb43e6fe3bd37ea
MarcosFernandesF <marcosrff15@gmail.com>
2025-12-08 02:01:07 -0300

    encapsulating the main logic from the board

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
-<script setup>
-import Board from './components/Board.vue'
-</script>
-
 <template>
   <Board />
 </template>
+
+<script setup>
+import Board from './components/Board.vue'
+
+</script>
--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,9 +1,6 @@
 <template>
   <header>
     <h1>TODO-BOARD</h1>
-    <button @click="clearStorage">
-       Clear Storage
-    </button>
   </header>
 
   <NewTaskInput 
@@ -14,7 +11,7 @@
 
   <main class="board-columns">
     <section class="board-column">
-      <BoardColumn title="To Do" @task-dropped="onTaskDrop($event, 'todo')">
+      <BoardColumn title="To Do" @task-dropped="onTaskDrop($event, TASK_STATUS.TODO)">
         <TaskCard 
           v-for="item in todoListSorted" 
           :key="item.id" 
@@ -24,7 +21,7 @@
       </BoardColumn>
     </section>
     <section>
-      <BoardColumn title="Done" @task-dropped="onTaskDrop($event, 'done')">
+      <BoardColumn title="Done" @task-dropped="onTaskDrop($event, TASK_STATUS.DONE)">
         <TaskCard 
           v-for="item in doneListSorted" 
           :key="item.id" 
@@ -37,89 +34,20 @@
 </template>
 
 <script setup>
-import { ref, computed, watch, onMounted } from 'vue';
 import BoardColumn from './BoardColumn.vue';
-import TaskCard from './TaskCard.vue'
+import TaskCard from './TaskCard.vue';
 import NewTaskInput from './NewTaskInput.vue';
 
-const STORAGE_KEY_TODO = 'todo-items';
-const STORAGE_KEY_DONE = 'done-items';
+import { useTaskBoard } from '@/composables/useTaskBoard';
+import { TASK_STATUS } from '@/constants/TaskConstants';
 
-const todoItems = ref([]);
-const doneItems = ref([]);
-
-const listMap = {
-  'todo': todoItems,
-  'done': doneItems
-};
-
-const todoListSorted = computed(() => {
-  return [...todoItems.value].sort((a, b) => b.id - a.id);
-});
-
-const doneListSorted = computed(() => {
-  return [...doneItems.value].sort((a, b) => b.id - a.id);
-});
-
-watch([todoItems, doneItems], () => {
-  localStorage.setItem(STORAGE_KEY_TODO, JSON.stringify(todoItems.value));
-  localStorage.setItem(STORAGE_KEY_DONE, JSON.stringify(doneItems.value));
-}, { deep: true });
-
-onMounted(() => {
-  const storedTodo = localStorage.getItem(STORAGE_KEY_TODO);
-  const storedDone = localStorage.getItem(STORAGE_KEY_DONE);
-
-  if (storedTodo) todoItems.value = JSON.parse(storedTodo);
-  if (storedDone) doneItems.value = JSON.parse(storedDone);
-});
-
-const removeTaskFromList = (status, taskId) => {
-  const list = listMap[status];
-  if (!list) return null;
-
-  const index = list.value.findIndex(t => t.id === taskId);
-  if (index !== -1) {
-    return list.value.splice(index, 1)[0];
-  }
-  return null;
-}
-
-const addTaskToList = (status, task) => {
-  const list = listMap[status];
-  if (list && task) {
-    task.status = status;
-    list.value.push(task);
-  }
-}
-
-const createNewTask = (newTask) => {
-  addTaskToList(newTask.status, newTask);
-}
-
-const deleteTask = (task) => {
-  if (confirm(`Are you sure that you want to delete "${task.title}"?`)) {
-    removeTaskFromList(task.status, task.id);
-  }
-}
-
-const onTaskDrop = ({ taskID, originStatus }, targetStatus) => {
-  if (originStatus === targetStatus) return;
-
-  const id = Number(taskID);
-  const taskToMove = removeTaskFromList(originStatus, id);
-
-  if (taskToMove) {
-    addTaskToList(targetStatus, taskToMove);
-  }
-}
-
-const clearStorage = () => {
-    localStorage.removeItem(STORAGE_KEY_TODO);
-    localStorage.removeItem(STORAGE_KEY_DONE);
-    todoItems.value = [];
-    doneItems.value = [];
-}
+const { 
+  todoListSorted, 
+  doneListSorted, 
+  createNewTask, 
+  deleteTask, 
+  onTaskDrop, 
+} = useTaskBoard();
 </script>
 
 <style scoped>
