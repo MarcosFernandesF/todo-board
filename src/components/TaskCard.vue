ac034422729030cafdbfbedbf46d5a3856f6b15d
MarcosFernandesF <marcosrff15@gmail.com>
2025-12-08 01:25:05 -0300

    making the TaskCard draggable

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,17 +1,29 @@
 <template>
-  <li class="task-card">
+  <li 
+    class="task-card"
+    draggable="true"
+    @dragstart="onDragStart"
+  >
     <p>Title: {{ task.title }}</p>
     <p>CreatedAt: {{ task.createdAt }}</p>
   </li>
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   task: {
     type: Object,
     required: true
   }
 })
+
+const onDragStart = (event) => {
+  event.dataTransfer.effectAllowed = 'move';
+  event.dataTransfer.dropEffect = 'move';
+  
+  event.dataTransfer.setData('taskID', props.task.id);
+  event.dataTransfer.setData('originStatus', props.task.status);
+}
 </script>
 
 <style scoped>
@@ -22,5 +34,10 @@ defineProps({
   list-style: none;
   padding: 10px;
   margin-bottom: 8px;
+  cursor: grab;
+}
+
+.task-card:active {
+  cursor: grabbing;
 }
 </style>
